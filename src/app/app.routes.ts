913fc8b7a273d72eb29abf9c9906bea0c81d5b2e
Jared Bada <official.jared.bada@gmail.com>
2024-06-21 16:32:42 +0300

    Initial Setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { LayoutComponent } from '@core/layout/layout.component';
+import { NotFoundComponent } from '@shared/not-found/not-found.component';
+
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', redirectTo: '/home', pathMatch: 'full' },
+    ],
+  },
+  { path: '**', component: NotFoundComponent },
+];
---
+++ src/app/core/layout/layout.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'layout',
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.scss',
+  standalone: true,
+  imports: [],
+})
+export class LayoutComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+}
