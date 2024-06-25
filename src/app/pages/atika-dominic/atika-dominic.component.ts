0e8bcac7ef298d739ad13737bc3c642e2c5f652a
Jared Bada <official.jared.bada@gmail.com>
2024-06-25 16:25:43 +0300

    more changes

--- src/app/pages/atika-dominic/atika-dominic.component.ts
+++ src/app/pages/atika-dominic/atika-dominic.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 
+import { GridContainerDirective } from '@shared/directives/grid-container/grid-container.directive';
+
 @Component({
   selector: 'adb-atika-dominic',
   standalone: true,
   imports: [],
   templateUrl: './atika-dominic.component.html',
-  styleUrl: './atika-dominic.component.scss'
+  styleUrl: './atika-dominic.component.scss',
 })
-export class AtikaDominicComponent {
+export class AtikaDominicComponent extends GridContainerDirective {
+  override ngOnInit(): void {
+    super.ngOnInit();
+  }
 
+  override setDefaultMetaAndTitle(): void {}
+  override setTwitterCardMeta(): void {}
+  override setFacebookOpenGraphMeta(): void {}
 }
